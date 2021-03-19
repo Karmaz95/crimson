858421f7d3b112fd3d2dc800e54b374320640616
karmaz95 <karol.mazurek95@gmail.com>
2021-03-19 23:31:15 +0100

    Patch

---
+++ scripts/crimson_backup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+#
+### CREATED BY KARMAZ
+#
+#
+### FUNCTIONS:
+#
+# 1. CHECK BACKUPS OF KNOWN FILES
+#
+###
+# USAGE EXAMPLE:
+#
+#
+# PAYLOAD LINE PROPER FORMAT:
+#   [payload]vps_ip
+#   [PAYLOAD]domain_collab
+###
+
