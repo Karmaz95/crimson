73ebc6852a1a3cbdbf754d81cf215eddf7f0f580
kraczak <kornel.raczak@gmail.com>
2021-03-20 21:14:30 +0100

    little refactor to crimson_backuper.py

--- scripts/crimson_backuper.py
+++ scripts/crimson_backuper.py
@@ -14,16 +14,16 @@
 
 import sys, getopt, requests, re
 from collections import Counter
+from http.cookies import SimpleCookie
 
 ### OPTIONS ---
-full_cmd_arguments = sys.argv
-argument_list = full_cmd_arguments[1:]
+argument_list = sys.argv[1:]
 short_options = "w:c:h:e:"
 long_options = ["wordlist", "cookies", "header", "extensions"]
 try:
-        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
 except getopt.error as err:
-        sys.exit(2)
+    sys.exit(2)
 ### --- (They will be iterated at the bottom of the screen ---
 
 def helper():
@@ -37,30 +37,24 @@ def helper():
 ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝\033[0m""")
     print("\nUSAGE: python crimson_backuper.py -w [wordlist_with_urls] -h [optional_headers]")
 
-def import_wordlist(wordlist):
+def import_wordlist(wordlist_filepath):
     '''Importing wordlist line by line into an array'''
-    with open(wordlist) as wordlist:
-        new_wordlist = [",".join(line.split(",")).strip() for line in wordlist.readlines()]
+    with open(wordlist_filepath) as f:
+        new_wordlist = [",".join(line.split(",")).strip() for line in f.readlines()]
     return new_wordlist
 
-def import_cookies(cookie):
+def import_cookies(cookie_data):
     '''Importing cookies from header f.e. "Cookie: auth1=qwe; auth2=asd;" '''
-    cookies = {}
-    #cookie_header = cookie.split(":")[0]
-    cookie_values = cookie.split(":")[1].split(";")[:-1]
-    for q in cookie_values:
-        cookies.update(dict([q.lstrip().split("=")]))
+    cookie = SimpleCookie()
+    cookie.load(cookie)
+    cookies = dict((key, morsel.value) for key, morsel in cookie.items())
     return cookies
 
-def combine_wordlists(wordlist1,wordlist2):
+def combine_wordlists(wordlist1, wordlist2):
     '''Join strings one by one from array1 with array2'''
-    new_wordlist = []
-    for word1 in wordlist1:
-        for word2 in wordlist2:
-            new_wordlist.append(word1+word2)
-    return new_wordlist
+    return [w1 + w2 for w1 in wordlist1 for w2 in wordlist2]
 
-def check_backup_file(wordlist1,wordlist2,cookies,headers):
+def check_backup_file(wordlist1, wordlist2, cookies, headers):
     '''Combine urls with extension wordlist and check for backup files'''
     s = requests.Session()
     s.cookies.update(cookies)
@@ -81,11 +75,12 @@ for current_argument, current_value in arguments:
         list_of_extensions = current_value
 
 ### MAIN
-try:
-    urls = import_wordlist(list_of_urls)
-    extensions = import_wordlist(list_of_extensions)
-    cookies = import_cookies(cookies)
-    backups = combine_wordlists(urls,extensions)
-    check_backup_file(urls,backups,cookies,headers)
-except:
-    helper()
+if __name__ == '__main__':
+    try:
+        urls = import_wordlist(list_of_urls)
+        extensions = import_wordlist(list_of_extensions)
+        cookies = import_cookies(cookies)
+        backups = combine_wordlists(urls, extensions)
+        check_backup_file(urls, backups, cookies, headers)
+    except:
+        helper()
