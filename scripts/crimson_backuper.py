2d25949669fcf65421983949ece56211a0b55577
karmaz95 <karol.mazurek95@gmail.com>
2021-03-21 15:47:43 +0100

    Patch

--- scripts/crimson_backuper.py
+++ scripts/crimson_backuper.py
@@ -3,15 +3,12 @@
 ### CREATED BY KARMAZ
 #
 #
-### TO DO:
-#
-# 1. Add MIME type check.
-#
 ### FUNCTIONS:
 #
 # 1. CHECK BACKUPS OF KNOWN FILES
 #
 ###
+#
 # USAGE EXAMPLE:
 #
 ###
@@ -23,11 +20,12 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 ### OPTIONS ---
 argument_list = sys.argv[1:]
-short_options = "w:c:h:e:"
-long_options = ["wordlist", "cookies", "header", "extensions"]
+short_options = "w:c:H:e:h"
+long_options = ["wordlist", "cookies", "header", "extensions", "help"]
 try:
     arguments, values = getopt.getopt(argument_list, short_options, long_options)
 except getopt.error as err:
+    print(err)
     sys.exit(2)
 ### --- (They will be iterated at the bottom of the screen ---
 
@@ -40,9 +38,9 @@ def helper():
 ██╔══██╗██╔══██║██║     ██╔═██╗ ██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗
 ██████╔╝██║  ██║╚██████╗██║  ██╗╚██████╔╝██║     ███████╗██║  ██║
 ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝\033[0m""")
-    print("\nUSAGE: python crimson_backuper.py -w [wordlist_with_urls] -h [optional_headers]")
+    print("\nUSAGE: python crimson_backuper.py -w [wordlist_with_urls] -H [optional_headers] -h [show help]")
 
-def import_wordlist(wordlist_filepath):
+def load_wordlist(wordlist_filepath):
     '''Importing wordlist line by line into an array'''
     with open(wordlist_filepath) as f:
         new_wordlist = [",".join(line.split(",")).strip() for line in f.readlines()]
@@ -61,50 +59,55 @@ def combine_wordlists(wordlist1, wordlist2):
     '''Join strings one by one from array1 with array2'''
     return [w1 + w2 for w1 in wordlist1 for w2 in wordlist2]
 
-def check_backup_file(urls, backups, cookies, headers):
+def check_backup_file(urls, extensions, cookies, headers):
     '''Combine urls with extension wordlist and check for backup files'''
     s = requests.Session()
     s.cookies.update(cookies)
     s.headers.update(headers)
     for url in urls:
-        for backup in backups:
+        r1 = s.get(url, allow_redirects=True, verify=False)
+        print("url:" + url)
+        for ext in extensions:
+            backup = url + ext
             r2 = s.get(backup, allow_redirects=True, verify=False)
+            print("\tbackup:" + backup)
             # Check if r2 is not permanently redirected, if not send r1.
             if not r2.is_permanent_redirect:
-                r1 = s.get(url, allow_redirects=True, verify=False)
-                # If status code of r2 is the same as r1, or status code of r2 is equal 200 step into next check.
-                if r2.status_code == r1.status_code or r2.status_code == 200:
+                # If status code of r2 is the same as r1, or status code of r2 is equal 200 step into next check and if there is not a redirection step into next check.
+                if r2.status_code == r1.status_code or r2.status_code == 200 and r2.status_code not in [301,302]:
                     # If Content-Length or the r2 is not the same as r1 step ino next check.
-                    if r2.headers["Content-Length"] != r1.headers["Content-Length"]:
-                        # If there is not a redirection step into next check.
-                        if r2.status_code != 301 or r2.status_code != 302:
-                            # If there are no reflection, the backup file has been found, either way there is a path/host reflection - check for xss.
-                            if r2.url not in r2.text:
-                                print("\033[0;31m [+]\033[0m BACKUP FILE FOUND AT: " + r2.url)
-                            else:
-                                print("\033[0;31m [+]\033[0m REFLECTION FOUND AT: " + r2.ulr)
+                    if r2.content != r1.content:
+                        # If there are no reflection, the backup file has been found, either way there is a path/host reflection - check for xss.
+                        if r2.url not in r2.text:
+                            print("\033[0;31m [+]\033[0m BACKUP FILE FOUND AT: " + r2.url)
+                        else:
+                            print("\033[0;31m [+]\033[0m REFLECTION FOUND AT: " + r2.ulr)
 
 
 ### OPTIONS ---
 headers = {}
+cookies ={}
+show_help = False
 for current_argument, current_value in arguments:
     if current_argument in ("-w", "--wordlist"):
         list_of_urls = current_value
     elif current_argument in ("-c", "--cookies"):
         cookies = current_value
-    elif current_argument in ("-h", "--header"):
+    elif current_argument in ("-H", "--header"):
         headers.update([current_value.split("=")])
     elif current_argument in ("-e", "--extensions"):
         list_of_extensions = current_value
+    elif current_argument in ("-h", "--help"):
+        show_help = True
 
 ### MAIN
 if __name__ == '__main__':
-    try:
-        urls = import_wordlist(list_of_urls)
-        extensions = import_wordlist(list_of_extensions)
-        backups = combine_wordlists(urls, extensions)
+    if show_help:
+        helper()
+    else:
+        print(1111)
+        urls = load_wordlist(list_of_urls)
+        extensions = load_wordlist(list_of_extensions)
         if cookies:
             cookies = import_cookies(cookies)
-        check_backup_file(urls, backups, cookies, headers)
-    except:
-        helper()
+        check_backup_file(urls, extensions, cookies, headers)
